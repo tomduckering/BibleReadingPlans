35625f79901092836f440eea5a57181094f87efc
Tom Duckering <tduckeri@thoughtworks.com>
2017-01-22 14:48:46 +0000

    Initial Commit

---
+++ BibleReadingPlans/BibleReadingPlans.h
@@ -0,0 +1,19 @@
+//
+//  BibleReadingPlans.h
+//  BibleReadingPlans
+//
+//  Created by Tom Duckering on 22/01/2017.
+//  Copyright © 2017 Tom Duckering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BibleReadingPlans.
+FOUNDATION_EXPORT double BibleReadingPlansVersionNumber;
+
+//! Project version string for BibleReadingPlans.
+FOUNDATION_EXPORT const unsigned char BibleReadingPlansVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BibleReadingPlans/PublicHeader.h>
+
+
